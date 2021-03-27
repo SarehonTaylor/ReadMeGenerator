985d89bfe7988cdab18ef8359a4859123cedc60d
Sarehon Taylor <sarehontaylor@icloud.com>
2021-03-26 21:55:28 -0400

    setting up index.js for questions array

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,14 @@ const fs = require('fs)');
 const utli = require('util');
 const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+
+
+
+
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
