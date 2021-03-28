8a8dc3b536aaf2697f6e8bbe904589fed2d9270d
Sarehon Taylor <sarehontaylor@icloud.com>
2021-03-28 13:02:31 -0400

    fixed logic with description preventing it from working correctly

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,32 @@
-function generateMarkdown(data) {
+// function to generate markdown for README
+function generateMarkdown(response) {
   return `
-# ${data.title}
-`;
-}
+# ${response.Title}
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Questions](#questions)
+## Description
+${response.Description}
+## Installation
+${response.Install}
+## Usage
+${response.Usage}
+## License
+${response.License}
+## Contributors
+${response.Contributors}
+## Test
+${response.Test}
+## Questions
+If you want to contact me:
+Github:[${response.Github}]
+Email:[${response.Email}]
+`;}
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
